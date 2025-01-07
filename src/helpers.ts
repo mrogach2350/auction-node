8b93550ccf1635765f5d5bb63aec558a018a0c53
Michael Rogachevsky <m.rogach@gmail.com>
2025-01-06 23:47:22 -0800

    adds bulk bid scraper

--- src/helpers.ts
+++ src/helpers.ts
@@ -7,3 +7,15 @@ export function isEmpty(obj: {}) {
 
   return true;
 }
+
+export function secondsToHms(d: number) {
+  d = Number(d);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor((d % 3600) / 60);
+  var s = Math.floor((d % 3600) % 60);
+
+  var hDisplay = h > 0 ? h + "hr" : "";
+  var mDisplay = m > 0 ? m + "m" : "";
+  // var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+  return !!mDisplay ? `${hDisplay}:${mDisplay}` : hDisplay;
+}
