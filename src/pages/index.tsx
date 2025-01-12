d8b7adf5ee51de93f6d49df5d1368d5410df105d
Michael Rogachevsky <m.rogach@gmail.com>
2025-01-12 13:06:06 -0800

    abstracts out Mobile and Desktop controls

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,21 +14,16 @@ import {
   colorSchemeDarkBlue,
   SelectionChangedEvent,
 } from "ag-grid-community";
-import { Button } from "react-bulma-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRotateLeft } from "@fortawesome/free-solid-svg-icons";
 import NoteModal from "@/components/NoteModal";
+import MobileControls from "@/components/MobileControls";
+import DesktopControls from "@/components/DesktopControls";
 import { getColDefs } from "@/helpers";
 import { getAllVehiclesQuery } from "@/queries";
 import {
   useDeleteVehiclesMutation,
   useGetOfferMutation,
   useAuctionScraperMutation,
-  useUndoDeleteVehiclesMutation,
-  useGetAuctionBidsMutation,
 } from "@/mutations";
-import ListDropdown from "@/components/ListsDropdown";
-import AddToListDropDown from "@/components/AddToListDropdown";
 
 const myTheme = themeQuartz.withPart(colorSchemeDarkBlue);
 
@@ -39,17 +34,11 @@ export default function Home({ isMobile }: { isMobile: boolean }) {
   const getOfferMutation = useGetOfferMutation();
   const deleteVehiclesMutation = useDeleteVehiclesMutation();
   const auctionScraperMutation = useAuctionScraperMutation();
-  const getAuctionBidsMutation = useGetAuctionBidsMutation();
-  const undoDeleteVehiclesMutation = useUndoDeleteVehiclesMutation();
-  const [scraperUrl, setScrapeUrl] = useState<string>("");
   const [selectedNodes, setSelectedNodes] = useState<any[]>([]);
   const [showNoteModal, setShowNoteModal] = useState<boolean>(false);
   const [selectedVehicle, setSelectedVehicle] = useState<any>({});
   const [gettingOfferId, setGettingOfferId] = useState<any>(null);
   const [selectedListId, setSelectedListId] = useState<number>(0);
-  const [lastDeletedVehicleIds, setLastDeletedVehicleIds] = useState<number[]>(
-    []
-  );
 
   const onGetOffer = (node: any) => {
     const { vin, mileage, id } = node.data;
@@ -131,42 +120,6 @@ export default function Home({ isMobile }: { isMobile: boolean }) {
     setShowNoteModal(false);
   };
 
-  const handleListChange = (id: any) => {
-    setSelectedListId(id);
-  };
-
-  const onAddVehicleToList = (listId: number) => {
-    gridRef?.current?.api.setFilterModel(null);
-    gridRef?.current?.api.deselectAll();
-    setSelectedListId(listId);
-  };
-
-  const handleDeleteVehicles = async () => {
-    const vehicleIds: number[] = selectedNodes.map((n) => n?.data?.id);
-    deleteVehiclesMutation.mutate(
-      { vehicleIds },
-      {
-        onSuccess: ({ deleteVehicleIds = [] }) => {
-          setLastDeletedVehicleIds(deleteVehicleIds);
-          queryClient.invalidateQueries({ queryKey: ["vehicles"] });
-          setSelectedNodes([]);
-        },
-      }
-    );
-  };
-
-  const handleUndoDelete = async () => {
-    undoDeleteVehiclesMutation.mutate(
-      { vehicleIds: lastDeletedVehicleIds },
-      {
-        onSuccess: () => {
-          setLastDeletedVehicleIds([]);
-          queryClient.invalidateQueries({ queryKey: ["vehicles"] });
-        },
-      }
-    );
-  };
-
   const showLoadingBar =
     areVehiclesByListLoading ||
     areVehiclesLoading ||
@@ -184,161 +137,21 @@ export default function Home({ isMobile }: { isMobile: boolean }) {
       )}
       <h1 className="title">Vehicles</h1>
       {isMobile ? (
-        <div className="space-y-1">
-          <div className="flex items-end space-x-2">
-            <div>
-              <label htmlFor="scraperUrl" className="subtitle flex-none">
-                Scrape Url
-              </label>
-              <input
-                name="scraperUrl"
-                className="input"
-                value={scraperUrl}
-                onChange={(e) => setScrapeUrl(e.target.value)}
-              />
-            </div>
-            <button
-              onClick={() =>
-                auctionScraperMutation.mutate(
-                  { scraperUrl },
-                  {
-                    onSuccess: () => {
-                      queryClient.invalidateQueries({ queryKey: ["vehicles"] });
-                      setScrapeUrl("");
-                    },
-                  }
-                )
-              }
-              className="button is-primary"
-              disabled={!scraperUrl.trim() || auctionScraperMutation.isPending}>
-              {auctionScraperMutation.isPending ? "Loading..." : "Submit"}
-            </button>
-          </div>
-          <button
-            className="button is-primary"
-            disabled={!selectedNodes.length || getAuctionBidsMutation.isPending}
-            onClick={() =>
-              getAuctionBidsMutation.mutate(
-                { selectedNodes },
-                {
-                  onSuccess: () => {
-                    queryClient.invalidateQueries({ queryKey: ["vehicles"] });
-                    setSelectedNodes([]);
-                  },
-                }
-              )
-            }>
-            {getAuctionBidsMutation.isPending
-              ? "Loading..."
-              : "Get Bids for Selected Rows"}
-          </button>
-          <div className="flex space-x-1 mb-1">
-            <ListDropdown
-              selectedListId={selectedListId}
-              onChange={handleListChange}
-            />
-            <AddToListDropDown
-              selectedVehicleNodes={selectedNodes}
-              onSave={onAddVehicleToList}
-            />
-            <button
-              className="button is-danger is-small"
-              disabled={
-                !selectedNodes.length || deleteVehiclesMutation.isPending
-              }
-              onClick={handleDeleteVehicles}>
-              {deleteVehiclesMutation.isPending ? "Loading..." : "Delete"}
-            </button>
-            <Button
-              disabled={lastDeletedVehicleIds.length === 0}
-              onClick={handleUndoDelete}
-              color="warning">
-              <span>
-                <FontAwesomeIcon icon={faArrowRotateLeft} />
-              </span>
-            </Button>
-          </div>
-        </div>
+        <MobileControls
+          gridRef={gridRef}
+          selectedNodes={selectedNodes}
+          setSelectedNodes={setSelectedNodes}
+          selectedListId={selectedListId}
+          setSelectedListId={setSelectedListId}
+        />
       ) : (
-        <div>
-          <div className="flex space-x-2 w-1/2 items-baseline">
-            <label htmlFor="scraperUrl" className="subtitle flex-none">
-              Scrape Url
-            </label>
-            <input
-              name="scraperUrl"
-              className="input"
-              value={scraperUrl}
-              onChange={(e) => setScrapeUrl(e.target.value)}
-            />
-            <button
-              onClick={() =>
-                auctionScraperMutation.mutate(
-                  { scraperUrl },
-                  {
-                    onSuccess: () => {
-                      queryClient.invalidateQueries({ queryKey: ["vehicles"] });
-                      setScrapeUrl("");
-                    },
-                  }
-                )
-              }
-              className="button is-primary"
-              disabled={!scraperUrl.trim() || auctionScraperMutation.isPending}>
-              {auctionScraperMutation.isPending ? "Loading..." : "Submit"}
-            </button>
-          </div>
-          <div className="flex justify-between">
-            <ListDropdown
-              selectedListId={selectedListId}
-              onChange={handleListChange}
-            />
-            <div className="flex justify-end space-x-2 mb-2">
-              <AddToListDropDown
-                selectedVehicleNodes={selectedNodes}
-                onSave={onAddVehicleToList}
-              />
-              <button
-                className="button is-primary"
-                disabled={
-                  !selectedNodes.length || getAuctionBidsMutation.isPending
-                }
-                onClick={() =>
-                  getAuctionBidsMutation.mutate(
-                    { selectedNodes },
-                    {
-                      onSuccess: () => {
-                        queryClient.invalidateQueries({
-                          queryKey: ["vehicles"],
-                        });
-                        setSelectedNodes([]);
-                      },
-                    }
-                  )
-                }>
-                {getAuctionBidsMutation.isPending
-                  ? "Loading..."
-                  : "Get Bids for Selected Rows"}
-              </button>
-              <button
-                className="button is-danger"
-                disabled={
-                  !selectedNodes.length || deleteVehiclesMutation.isPending
-                }
-                onClick={handleDeleteVehicles}>
-                {deleteVehiclesMutation.isPending ? "Loading..." : "Delete"}
-              </button>
-              <Button
-                disabled={lastDeletedVehicleIds.length === 0}
-                onClick={handleUndoDelete}
-                color="warning">
-                <span>
-                  <FontAwesomeIcon icon={faArrowRotateLeft} />
-                </span>
-              </Button>
-            </div>
-          </div>
-        </div>
+        <DesktopControls
+          gridRef={gridRef}
+          selectedNodes={selectedNodes}
+          setSelectedNodes={setSelectedNodes}
+          selectedListId={selectedListId}
+          setSelectedListId={setSelectedListId}
+        />
       )}
       <div className="h-5/6">
         <AgGridReact
